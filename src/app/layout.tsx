e407dee455283a6430492782b3f1faa7d2dd0e26
ptrv-dev <petrov99119@gmail.com>
2023-11-09 11:32:51 +0200

    shadcn-ui: toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 
 import { cn } from '@/lib/utils';
 import AuthProvider from '@/components/AuthProvider';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -29,6 +30,7 @@ export default function RootLayout({
         )}
       >
         <AuthProvider>{children}</AuthProvider>
+        <Toaster />
       </body>
     </html>
   );
