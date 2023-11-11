a01d66d5eb5e0986765c7afd839fd0c06d7214e6
ptrv-dev <petrov99119@gmail.com>
2023-11-11 12:35:23 +0200

    components: write-post functionallity

--- src/components/WritePost.tsx
+++ src/components/WritePost.tsx
@@ -2,37 +2,106 @@
 
 import { FC, useState } from 'react';
 import { ImageIcon, VideoIcon } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import * as z from 'zod';
+
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
+import { Form, FormField, FormItem, FormMessage } from './ui/form';
+import { toast } from './ui/use-toast';
 import { cn } from '@/lib/utils';
+
+const formSchema = z.object({
+  text: z
+    .string({ required_error: 'Text is required' })
+    .min(4, 'Text must be at least 4 characters long')
+    .max(2048, 'Text must be at most 2048 characters long'),
+});
+type FormSchemaType = z.infer<typeof formSchema>;
 
 interface Props {
   className?: string;
 }
 
 const WritePost: FC<Props> = ({ className }) => {
-  const [text, setText] = useState('');
+  const form = useForm<FormSchemaType>({
+    defaultValues: { text: '' },
+    resolver: zodResolver(formSchema),
+  });
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (data: FormSchemaType) => {
+    if (loading) return;
+    try {
+      setLoading(true);
+      await axios.post(
+        '/api/post',
+        { text: data.text },
+        { withCredentials: true }
+      );
+      toast({
+        title: 'Post created',
+      });
+      form.reset();
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: 'Error',
+        description: 'Something went wrong, please try again later.',
+        variant: 'destructive',
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className={cn(className)}>
-      <Textarea
-        className="resize-none w-full"
-        placeholder="Write your post here..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        rows={4}
-      />
-      <div className="w-full mt-2 flex justify-between">
-        <div className="flex gap-2">
-          <Button size="icon" className="w-8 h-8" variant="link">
-            <ImageIcon size={20} />
-          </Button>
-          <Button size="icon" className="w-8 h-8" variant="link">
-            <VideoIcon size={20} />
+    <Form {...form}>
+      <form className={cn(className)} onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          name="text"
+          render={({ field }) => (
+            <FormItem>
+              <Textarea
+                className="resize-none w-full"
+                placeholder="Write your post here..."
+                rows={4}
+                disabled={loading}
+                {...field}
+              />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="w-full mt-2 flex justify-between">
+          <div className="flex gap-2">
+            <Button
+              type="button"
+              size="icon"
+              className="w-8 h-8"
+              variant="link"
+              disabled={loading}
+            >
+              <ImageIcon size={20} />
+            </Button>
+            <Button
+              type="button"
+              size="icon"
+              className="w-8 h-8"
+              variant="link"
+              disabled={loading}
+            >
+              <VideoIcon size={20} />
+            </Button>
+          </div>
+          <Button type="submit" className="flex" disabled={loading}>
+            Post
           </Button>
         </div>
-        <Button className="flex">Post</Button>
-      </div>
-    </div>
+      </form>
+    </Form>
   );
 };
 
