d396374c3ea3f6c1a993ed704c78d98ae5cedca8
ptrv-dev <petrov99119@gmail.com>
2023-11-11 14:51:37 +0200

    components: post-like-button

---
+++ src/components/PostLikeButton.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { HeartIcon, Loader2Icon } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import { FC, useState } from 'react';
+import { toast } from './ui/use-toast';
+import axios from 'axios';
+import { IGetPost } from '@/app/api/post/interface';
+import { cn } from '@/lib/utils';
+
+interface Props {
+  id: number;
+  likes: number[];
+}
+
+const PostLikeButton: FC<Props> = ({ id, likes }) => {
+  const [localLikes, setLocalLikes] = useState<number[]>(likes);
+  const [loading, setLoading] = useState<boolean>(false);
+  const session = useSession();
+
+  const handleLike = async () => {
+    if (session.status === 'loading') return;
+    if (session.status === 'unauthenticated')
+      return toast({ title: 'You should be logged in' });
+
+    setLoading(true);
+
+    try {
+      const { data } = await axios.post<IGetPost>(`/api/post/like/${id}`);
+      setLocalLikes(data.data.likes);
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: 'Error',
+        description: "Can't like post, please try again later..",
+        variant: 'destructive',
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleLike}
+      className="flex items-center gap-2 text-primary p-2 rounded hover:bg-primary-foreground transition-colors"
+    >
+      {loading ? (
+        <Loader2Icon size={20} className="animate-spin" />
+      ) : (
+        <>
+          <HeartIcon
+            className={cn({
+              'fill-primary': localLikes.includes(session.data?.user.id || -1),
+            })}
+            size={20}
+          />
+          {localLikes.length}
+        </>
+      )}
+    </button>
+  );
+};
+
+export default PostLikeButton;
