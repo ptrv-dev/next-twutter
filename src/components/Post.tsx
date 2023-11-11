6fc03da20a3ce97a4ba38b8f43d370e3dff39c63
ptrv-dev <petrov99119@gmail.com>
2023-11-11 16:19:45 +0200

    components: post add comment button

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,8 +1,9 @@
 import { cn } from '@/lib/utils';
 import { FC } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { MessageCircleIcon } from 'lucide-react';
 import PostLikeButton from './PostLikeButton';
+import PostCommentButton from './PostCommentButton';
+import { IComment } from '@/app/api/post/interface';
 
 interface Props {
   className?: string;
@@ -13,10 +14,19 @@ interface Props {
   };
   text: string;
   likes: number[];
+  comments: IComment[];
   createdAt: Date;
 }
 
-const Post: FC<Props> = ({ className, id, author, text, likes, createdAt }) => {
+const Post: FC<Props> = ({
+  className,
+  id,
+  author,
+  text,
+  likes,
+  comments,
+  createdAt,
+}) => {
   return (
     <div className={cn('flex', className)}>
       <Avatar className="mr-4">
@@ -33,9 +43,7 @@ const Post: FC<Props> = ({ className, id, author, text, likes, createdAt }) => {
         <p className="whitespace-pre-wrap">{text}</p>
         <div className="mt-2 flex items-center gap-2">
           <PostLikeButton id={id} likes={likes} />
-          <button className="flex items-center gap-2 text-primary p-2 rounded hover:bg-primary-foreground transition-colors">
-            <MessageCircleIcon size={20} />4
-          </button>
+          <PostCommentButton comments={comments} />
         </div>
       </div>
     </div>
